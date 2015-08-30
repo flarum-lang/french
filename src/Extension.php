34ccfb69d8c9c42a08397be3d6720c0aab878c5d
Maël Soucaze <maelsoucaze@gmail.com>
2015-08-30 07:26:24 +0200

    Use "Français" instead of "French".

--- src/Extension.php
+++ src/Extension.php
@@ -9,10 +9,10 @@ class Extension extends BaseExtension
     public function listen(Dispatcher $events)
     {
         $events->listen(RegisterLocales::class, function (RegisterLocales $event) {
-            $event->manager->addLocale('fr', 'French');
-            $event->addTranslations('fr', __DIR__.'/../locale/core.yml');
+            $event->manager->addLocale('fr', 'Français');
             $event->manager->addJsFile('fr', __DIR__.'/../locale/core.js');
             $event->manager->addConfig('fr', __DIR__.'/../locale/core.php');
+            $event->addTranslations('fr', __DIR__.'/../locale/core.yml');
             $event->addTranslations('fr', __DIR__.'/../locale/likes.yml');
             $event->addTranslations('fr', __DIR__.'/../locale/lock.yml');
             $event->addTranslations('fr', __DIR__.'/../locale/mentions.yml');
