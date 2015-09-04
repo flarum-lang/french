8f109f1e3d86d46b35935858bed57a37f4f94e37
Maël Soucaze <maelsoucaze@gmail.com>
2015-09-04 07:13:57 +0200

    Add copyright.
    https://github.com/flarum/tags/commit/a1e80138cb68e148395ff7e84f149b190ca546a5

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 // Require the extension's composer autoload file. This will enable all of our
 // classes in the src directory to be autoloaded.
--- src/Extension.php
+++ src/Extension.php
@@ -1,4 +1,13 @@
-<?php namespace Qia;
+<?php 
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Qia;
 
 use Flarum\Support\Extension as BaseExtension;
 use Illuminate\Events\Dispatcher;
