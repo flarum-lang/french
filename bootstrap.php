838b4c34117345a68409974b806d69490d44ea6c
Maël Soucaze <maelsoucaze@gmail.com>
2015-09-19 23:57:56 +0200

    Change to the correct copyright notice.

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
 /*
- * This file is part of Flarum.
+ * This file is part of the French Extension for Flarum.
  *
- * (c) Toby Zerner <toby.zerner@gmail.com>
+ * (c) Maël Soucaze <maelsoucaze@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
