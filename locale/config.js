0a76308f13efbfb3096032ea04c7744e1ef1eb5c
Maël Soucaze <maelsoucaze@gmail.com>
2016-01-01 20:30:49 +0100

    Extra spaces after keys.

--- locale/config.js
+++ locale/config.js
@@ -2,20 +2,20 @@
 // Based on: https://github.com/moment/moment/blob/develop/locale/fr.js
 
 moment.locale('fr', {
-  months : 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
-  monthsShort : 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
-  weekdays : 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
-  weekdaysShort : 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
-  weekdaysMin : 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
-  longDateFormat : {
-    LT : 'HH:mm',
-    LTS : 'HH:mm:ss',
-    L : 'DD/MM/YYYY', // Swiss French uses DD.MM.YYYY. Canadian French uses YYYY-MM-DD.
-    LL : 'D MMMM YYYY',
-    LLL : 'D MMMM YYYY HH:mm',
-    LLLL : 'dddd D MMMM YYYY HH:mm'
+  months: 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+  monthsShort: 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+  weekdays: 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
+  weekdaysShort: 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
+  weekdaysMin: 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+  longDateFormat: {
+    LT: 'HH:mm',
+    LTS: 'HH:mm:ss',
+    L: 'DD/MM/YYYY', // Swiss French uses DD.MM.YYYY. Canadian French uses YYYY-MM-DD.
+    LL: 'D MMMM YYYY',
+    LLL: 'D MMMM YYYY HH:mm',
+    LLLL: 'dddd D MMMM YYYY HH:mm'
   },
-  calendar : {
+  calendar: {
     sameDay: '[Aujourd’hui à] LT',
     nextDay: '[Demain à] LT',
     nextWeek: 'dddd [à] LT',
@@ -23,27 +23,27 @@ moment.locale('fr', {
     lastWeek: 'dddd [dernier à] LT',
     sameElse: 'L'
   },
-  relativeTime : {
-    future : 'dans %s',
-    past : 'il y a %s',
-    s : 'quelques secondes',
-    m : 'une minute',
-    mm : '%d minutes',
-    h : 'une heure',
-    hh : '%d heures',
-    d : 'un jour',
-    dd : '%d jours',
-    M : 'un mois',
-    MM : '%d mois',
-    y : 'un an',
-    yy : '%d ans'
+  relativeTime: {
+    future: 'dans %s',
+    past: 'il y a %s',
+    s: 'quelques secondes',
+    m: 'une minute',
+    mm: '%d minutes',
+    h: 'une heure',
+    hh: '%d heures',
+    d: 'un jour',
+    dd: '%d jours',
+    M: 'un mois',
+    MM: '%d mois',
+    y: 'un an',
+    yy: '%d ans'
   },
   ordinalParse: /\d{1,2}(er|e)/,
-  ordinal : function (number) {
+  ordinal: function (number) {
     return number + (number === 1 ? 'er' : 'e');
   },
-  week : {
-    dow : 1, // Monday is the first day of the week. Canadian French uses 0, because Sunday is the first day of the week.
-    doy : 4  // The week that contains Jan 4th is the first week of the year. Canadian French uses 6, because the week that contains Jan 1st is the first week of the year.
+  week: {
+    dow: 1, // Monday is the first day of the week. Canadian French uses 0, because Sunday is the first day of the week.
+    doy: 4  // The week that contains Jan 4th is the first week of the year. Canadian French uses 6, because the week that contains Jan 1st is the first week of the year.
   }
 });
