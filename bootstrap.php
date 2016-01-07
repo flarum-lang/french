8ad04d653f534edcd42f5ffea3a8c5b2ce250226
Maël Soucaze <maelsoucaze@gmail.com>
2016-01-07 13:23:22 +0100

    Normalize PHP file headers.

--- bootstrap.php
+++ bootstrap.php
@@ -5,8 +5,8 @@
  * @author Maël Soucaze <https://www.maelsoucaze.com>
  * @license MIT License <https://opensource.org/licenses/MIT>
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
  */
 
 use Flarum\Event\ConfigureLocales;
