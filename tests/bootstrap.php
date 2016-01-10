2644ced170d8bad0f25eeaffe05eb8bfab6c515d
Maël Soucaze <maelsoucaze@gmail.com>
2016-01-10 20:30:15 +0100

    Update PHP DocBlocks.
    Using recommendations from:
    http://manual.phpdoc.org/HTMLSmartyConverter/HandS/phpDocumentor/tutorial_tags.pkg.html

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * This file is part of the French language pack for Flarum.
+ * This file is part of the French Language Pack for Flarum.
  *
  * @author Maël Soucaze <https://www.maelsoucaze.com>
- * @license MIT License <https://opensource.org/licenses/MIT>
+ * @license https://opensource.org/licenses/MIT MIT License
  *
  * For the full copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
