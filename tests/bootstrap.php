57f1ace0379d1d7ea4be7e54828017ba354105f5
Maël Soucaze <maelsoucaze@gmail.com>
2016-01-11 16:04:09 +0100

    Remove header.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,3 @@
 <?php
-/**
- * This file is part of the French Language Pack for Flarum.
- *
- * @author Maël Soucaze <https://www.maelsoucaze.com>
- * @license https://opensource.org/licenses/MIT MIT License
- *
- * For the full copyright and license information, please view the
- * LICENSE file that was distributed with this source code.
- */
+
 require __DIR__.'/../vendor/autoload.php';
