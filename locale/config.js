340fd14de8373cf143ac050ca14de5fca2f8fa94
Qiaeru <22643035+qiaeru@users.noreply.github.com>
2020-11-19 17:39:48 +0100

    Migration from Moment.js to Day.js

--- locale/config.js
+++ locale/config.js
@@ -1,39 +1,26 @@
-// Custom French Moment.js Locales
-// Based on: https://github.com/moment/moment/blob/develop/locale/fr.js
+// Based on: https://github.com/iamkun/dayjs/blob/dev/src/locale/fr.js
 
-moment.locale('fr', {
-  months: 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split(
-    '_'
-  ),
-  monthsShort: 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split(
-    '_'
-  ),
-  monthsParseExact: true,
+const locale = {
+  name: 'fr',
   weekdays: 'dimanche_lundi_mardi_mercredi_jeudi_vendredi_samedi'.split('_'),
   weekdaysShort: 'dim._lun._mar._mer._jeu._ven._sam.'.split('_'),
   weekdaysMin: 'di_lu_ma_me_je_ve_sa'.split('_'),
-  weekdaysParseExact: true,
-  longDateFormat: {
+  months: 'janvier_février_mars_avril_mai_juin_juillet_août_septembre_octobre_novembre_décembre'.split('_'),
+  monthsShort: 'janv._févr._mars_avr._mai_juin_juil._août_sept._oct._nov._déc.'.split('_'),
+  weekStart: 1,
+  yearStart: 4,
+  formats: {
     LT: 'HH:mm',
     LTS: 'HH:mm:ss',
     L: 'DD/MM/YYYY',
     LL: 'D MMMM YYYY',
     LLL: 'D MMMM YYYY HH:mm',
-    LLLL: 'dddd D MMMM YYYY HH:mm',
-  },
-  calendar: {
-    sameDay: '[Aujourd’hui à] LT',
-    nextDay: '[Demain à] LT',
-    nextWeek: 'dddd [à] LT',
-    lastDay: '[Hier à] LT',
-    lastWeek: 'dddd [dernier à] LT',
-    sameElse: 'L',
+    LLLL: 'dddd D MMMM YYYY HH:mm'
   },
   relativeTime: {
     future: 'dans %s',
     past: 'il y a %s',
     s: 'quelques secondes',
-    ss: '%d secondes',
     m: 'une minute',
     mm: '%d minutes',
     h: 'une heure',
@@ -43,33 +30,10 @@ moment.locale('fr', {
     M: 'un mois',
     MM: '%d mois',
     y: 'un an',
-    yy: '%d ans',
+    yy: '%d ans'
   },
-  dayOfMonthOrdinalParse: /\d{1,2}(er|)/,
-  ordinal: function (number, period) {
-    switch (period) {
-      // TODO: Return 'e' when day of month > 1. Move this case inside
-      // block for masculine words below.
-      // See https://github.com/moment/moment/issues/3375
-      case 'D':
-        return number + (number === 1 ? 'er' : '');
-
-      // Words with masculine grammatical gender: mois, trimestre, jour
-      default:
-      case 'M':
-      case 'Q':
-      case 'DDD':
-      case 'd':
-        return number + (number === 1 ? 'er' : 'e');
-
-      // Words with feminine grammatical gender: semaine
-      case 'w':
-      case 'W':
-        return number + (number === 1 ? 're' : 'e');
-    }
-  },
-  week: {
-    dow: 1, // Monday is the first day of the week.
-    doy: 4, // The week that contains Jan 4th is the first week of the year.
-  },
-});
+  ordinal: (n) => {
+    const o = n === 1 ? 'er' : ''
+    return `${n}${o}`
+  }
+}
